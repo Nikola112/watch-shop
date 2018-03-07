6c4e810a150f2e979120e6742fbcb3061e1eac2c
Nikola Jovic <nikjovke1@gmail.com>
2018-03-07 20:39:03 +0100

    Navbar hide

--- js/scrolltotop.js
+++ js/scrolltotop.js
@@ -1,4 +1,45 @@
-function topFunction() 
+$(document).ready(function()
 {
-    $('html, body').animate({scrollTop : 0},500);
-}
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 150;
+    var navbarHeight = $('#main-navigation').outerHeight();
+
+    $(window).scroll(function(event)
+    {
+        didScroll = true;
+    });
+
+    setInterval(function()
+    {
+        if (didScroll && $(window).width() < 991.98)
+        {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled()
+    {
+        var st = $(this).scrollTop();
+
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight)
+        {
+            $('#main-navigation').addClass('nav-up');
+        }
+        else if(st + $(window).height() < $(document).height())
+        {
+            $('#main-navigation').removeClass('nav-up');
+        }
+        
+        lastScrollTop = st;
+    }
+
+    $(".scrollToTop").click(function()
+    {
+        $('html, body').animate({scrollTop : 0}, 600);
+    });
+});
